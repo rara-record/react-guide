a31cff29126a6df5aa05e250034e9ed9080769af
BORA <a52447879@gmail.com>
2021-12-01 20:11:50 +0900

    item컴포넌트 상위 list 컴포넌트 추가

--- 3. 렌더링 리스트 및 조건부 content/src/components/Expenses/Expenses.js
+++ 3. 렌더링 리스트 및 조건부 content/src/components/Expenses/Expenses.js
@@ -1,50 +1,30 @@
 import React, { useState } from 'react';
-import ExpenseFilter from './ExpenseFilter';
-import ExpenseItem from './ExpenseItem';
+
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
+import ExpenseList from './ExpenseList';
 import './Expenses.css';
 
-function Expenses(props) {
-
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
 
-  const filterExpenses = props.items.filter((expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  }))
+  });
 
-  let expensesContent = <p>조건에 맞는 데이터가 없습니다.</p>
-
-  if (filterExpenses.length > 0) {
-    expensesContent =
-      filterExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))
-  }
-
-
-
-
-  // App.js에서 가져온 배열에서 데이터 추출
   return (
-    <>
-      <Card className="expenses">
-        <ExpenseFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        {expensesContent}
-      </Card>
-    </>
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpenseList items={filteredExpenses} />
+    </Card>
   );
-}
+};
 
 export default Expenses;
