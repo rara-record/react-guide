7d40625f555bf8cf06f77f83d1c480bc79f28b97
BORA <a52447879@gmail.com>
2021-12-01 17:05:32 +0900

    app.js에서 가져온 배열에서 map함수를 이용해 데이터 추출

---
+++ 3. 렌더링 리스트 및 조건부 content/src/components/NewExpenses/ExpenseForm.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  // state를 활용하여 value 저장하기
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  // form이 제출됐을때
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    // 변경한 state(value)값을 오브젝트에 담는다
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    // 상위 컴포넌트로 state 올리기
+    props.onSaveExpenseData(expenseData);
+
+    // input 초기화
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler} />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            value={enteredAmount}
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler} />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={enteredDate}
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler} />
+        </div>
+
+      </div>
+
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+
+    </form>
+  );
+}
+
+export default ExpenseForm;
