d67c19efc16166c6d012b79b101ba25644455a1e
BORA <a52447879@gmail.com>
2021-12-01 13:53:05 +0900

    이전 state에 의존하는 state 업데이트 방법

--- 2. 리액트 state 및 이벤트/src/components/NewExpenses/ExpenseForm.js
+++ 2. 리액트 state 및 이벤트/src/components/NewExpenses/ExpenseForm.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  return (
 
+  // const [enteredTitle, setEnteredTitle] = useState('');
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  // const [enteredDate, setEnteredDate] = useState('');
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  });
+
+  const titleChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+  };
+
+  const amountChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
+  };
+
+  // 상태 업데이트가 이전 상태에 달려있는 상황일때는 
+  // 익명함수를 사용하여, prevState를 받아서 상태를 업데이트 한다.
+  // 이것이 가장 최근의 상태를 활용하도록, 보장하는 가장 안전한 방법이다
+
+  return (
     <form>
       <div className="new-expense__controls">
 
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
         </div>
 
       </div>
