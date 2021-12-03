d884f45e98657fbc1cf5d193693d79bf94397287
BORA <a52447879@gmail.com>
2021-12-03 17:09:39 +0900

    컴포넌트 스타일 인라인 스타일로 변경하기

--- 4. 리액트 컴포넌트 스타일링/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 4. 리액트 컴포넌트 스타일링/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -10,15 +10,23 @@ const CourseInput = props => {
     setEnteredValue(event.target.value);
   };
 
+  const [isValid, setIsValid] = useState(true);
+
   const formSubmitHandler = event => {
     event.preventDefault();
+
+    // 아무것도 입력하지 않았다면 return
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+    };
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
