f1915a998e038cb4d7012de2fc1ed6ae8a362457
BORA <a52447879@gmail.com>
2021-12-03 17:44:35 +0900

    동적으로 css추가하기

--- 4. 리액트 컴포넌트 스타일링/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 4. 리액트 컴포넌트 스타일링/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,28 +5,27 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
   };
 
-  const [isValid, setIsValid] = useState(true);
-
   const formSubmitHandler = event => {
     event.preventDefault();
 
-    // 아무것도 입력하지 않았다면 return
+    // 아무것도 입력하지 않았다면 (공백의 길이가 0)
     if (enteredValue.trim().length === 0) {
       setIsValid(false)
+      return;
     };
-
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''} `}>
+        <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
