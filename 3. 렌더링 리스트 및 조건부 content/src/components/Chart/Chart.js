1309be4cc8e1e6164ec38c8c73f4364c50dae27b
BORA <a52447879@gmail.com>
2021-12-04 14:34:45 +0900

    🔊 리액트 컴포넌트 스타일링 끝

--- 3. 렌더링 리스트 및 조건부 content/src/components/Chart/Chart.js
+++ 3. 렌더링 리스트 및 조건부 content/src/components/Chart/Chart.js
@@ -5,7 +5,6 @@ import './Chart.css';
 const Chart = (props) => {
   // data에서 가격만 추출
   const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-  // 차트에 나타날 실질적인 12달의 가격(제일 높은 가격)
   const totalMaximum = Math.max(...dataPointValues); // 배열이어서 spread 연산자사용
 
   return (
