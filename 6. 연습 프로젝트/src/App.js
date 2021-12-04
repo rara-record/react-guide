3a76cba618e01264dd01135438eb2f2d7499608e
BORA <a52447879@gmail.com>
2021-12-04 18:36:21 +0900

    📦 Add UserList.js output component

--- 6. 연습 프로젝트/src/App.js
+++ 6. 연습 프로젝트/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
+// input / output 로직으로 두개의 컴포넌트를 만든다.
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
