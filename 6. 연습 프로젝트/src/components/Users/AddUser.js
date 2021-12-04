e58c0e845fb33f341c3bdf89e365fa073814f203
BORA <a52447879@gmail.com>
2021-12-04 17:19:16 +0900

    📝 Update User form entered state

--- 6. 연습 프로젝트/src/components/Users/AddUser.js
+++ 6. 연습 프로젝트/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Card from '../UI/Card';
 import Button from '../UI/Button'
@@ -6,19 +6,45 @@ import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
 
+  // username, age state declare
+  const [enteredUsername, setEnteredUsername] = useState('')
+  const [enteredAge, setEnteredAge] = useState('')
+
   // 폼 제출 함수
   const addUserHandler = event => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge)
   }
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  }
+
+
+  // 두가지 값을 얻는다. username + age
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+        >
+        </input>
 
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+        >
+        </input>
 
         <Button type="submit">Add User</Button>
       </form>
