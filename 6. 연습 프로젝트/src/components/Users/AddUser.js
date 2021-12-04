5764ac70ccea42f2e521ccd431e5ca682fd1aad1
BORA <a52447879@gmail.com>
2021-12-04 17:06:49 +0900

    📝 Update AddUser.js + Button component

--- 6. 연습 프로젝트/src/components/Users/AddUser.js
+++ 6. 연습 프로젝트/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Card from '../UI/Card';
+import Button from '../UI/Button'
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
@@ -19,7 +20,7 @@ const AddUser = (props) => {
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number"></input>
 
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   )
